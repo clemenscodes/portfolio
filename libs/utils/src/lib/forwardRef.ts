061ca61ffc3480ec16a25854def3e62cd194a967
Clemens Horn <clemens.horn@mni.thm.de>
2023-03-30 19:11:58 +0200

    feat(utils): set default type for custom forwardRef, usage now very nice

--- libs/utils/src/lib/forwardRef.ts
+++ libs/utils/src/lib/forwardRef.ts
@@ -1,10 +1,11 @@
+import { ExtractElement } from '@types';
 import React from 'react';
 
 type Fn<T, P> = typeof React.forwardRef<T, P>;
 type Params<T, P> = Parameters<Fn<T, P>>;
 type Return<T, P> = ReturnType<Fn<T, P>>;
 
-type ForwardRefWithDisplayName = <T, P>(...args: [...Params<T, P>, string]) => Return<T, P>;
+type ForwardRefWithDisplayName = <P, T = ExtractElement<P>>(...args: [...Params<T, P>, string]) => Return<T, P>;
 
 // Extend reacts forwardRef to force setting a displayName
 export const forwardRef: ForwardRefWithDisplayName = (component, displayName) => {

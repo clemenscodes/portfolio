0ee5b11f576b31a938e6b84fb42b0190e2fb2026
Clemens Horn <clemens.horn@mni.thm.de>
2023-06-27 17:25:35 +0200

    refactor(web): remove fonts and providers lib

--- libs/providers/src/lib/font-provider/font-provider.tsx
+++
@@ -1,9 +0,0 @@
-import { RobotoCondensed } from '@fonts';
-
-export type FontProviderProps = React.PropsWithChildren;
-
-export const FontProvider: React.FC<FontProviderProps> = ({ children }) => {
-    return <RobotoCondensed>{children}</RobotoCondensed>;
-};
-
-export default FontProvider;
